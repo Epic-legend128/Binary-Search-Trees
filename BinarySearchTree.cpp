4ce44d14a0d25f5314e0d90f33da4e7ba966b202
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2025-03-22 11:10:55 +0100

    Changed printing + more comments

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
 #include <random>
+
+#define print(a) for(int x: a) std::cout << x<<' ';
 
 struct node {
     int data;
@@ -13,56 +16,71 @@ int min_height(node* root) {
     return 1 + std::min(min_height(root->left), min_height(root->right));
 }
 
-//this is like the max height, I just named it height because that is the normal height
+/* Returns height of the tree
+@param root root of the tree */
 int height(node* root) {
     if (root == nullptr) return 0;
     return 1 + std::max(height(root->left), height(root->right));
 }
 
-//finds node with biggest value in a binary search tree
+/* Returns maximum valued node
+@param root root of the tree */
 node* find_max(node* root) {
     if (root->right == nullptr) return root;
     return find_max(root->right);
 }
 
-//finds node with least value in a binary search tree
+/* Returns minimum valued node
+@param root root of the tree */
 node* find_min(node* root) {
     if (root->left == nullptr) return root;
     return find_min(root->left);
 }
 
 //this function finds a specific node in a binary search tree
-node* find_num(node* root, node* key) {
-    if (root->data > key->data) return find_num(root->left, key);
-    else if (root->data < key->data) return find_num(root->right, key);
+/* Searches and returns a node
+@param root root of the tree 
+@param key value that you are searching for*/
+node* find_num(node* root, int key) {
+    if (root->data > key) return find_num(root->left, key);
+    else if (root->data < key) return find_num(root->right, key);
     return root;
 }
 
-//Inorder(left, root, right)
-void print(node* root) {
+/* Inorder representation(left, root, right)
+@param root root of the tree
+@param arr vector which holds the inorder representation */
+void inorder(node* root, std::vector<int>& arr) {
     if (root == nullptr) return;
-    print(root->left);
-    std::cout << root->data<< "\n";
-    print(root->right);
+    inorder(root->left, arr);
+    arr.push_back(root->data);
+    inorder(root->right, arr);
 }
 
-//Preorder(root, left, right)
-void print_Preorder(node* root) {
+/* Preorder representation(root, left, right)
+@param root root of the tree
+@param arr vector which holds the preorder representation */
+void preorder(node* root, std::vector<int>& arr) {
     if (root == nullptr) return;
-    std::cout << root->data<< "\n";
-    print_Preorder(root->left);
-    print_Preorder(root->right);
+    arr.push_back(root->data);
+    preorder(root->left, arr);
+    preorder(root->right, arr);
 }
 
-//Postorder(left, right, root)
-void print_Postorder(node* root) {
+/* Postorder representation(left, right, root)
+@param root root of the tree
+@param arr vector which holds the postorder representation */
+void postorder(node* root, std::vector<int>& arr) {
     if (root == nullptr) return;
-    print_Postorder(root->left);
-    print_Postorder(root->right);
-    std::cout << root->data<< "\n";
+    postorder(root->left, arr);
+    postorder(root->right, arr);
+    arr.push_back(root->data);
 }
 
 //insert for binary search trees
+/* Insert node in BST
+@param root root of the tree
+@param p node to be inserted */
 void insert(node* &root, node* p) {
     if (p->data > root->data) {
         if (root->right == nullptr) {
@@ -82,13 +100,15 @@ void insert(node* &root, node* p) {
     }
 }
 
-//put as parameter any node of the binary search tree and this function will find the root of the tree, the node at the top of the tree
+/* Finds root of the tree
+@param p starting node */
 node* find_root(node* p) {
     if (p->parent==nullptr) return p;
     return find_root(p->parent);
 }
 
-//delete the given node
+/* Delete node from the BST
+@param n node to be deleted */
 void del(node* n) {
     if (n == nullptr) return;
 
@@ -155,14 +175,22 @@ int main() {
         std::cout << p[i]->data<<"\n";
     }
     std::cout << "\n";
+    std::vector<int> arr;
     std::cout << "The elements of the tree printed using Inorder:\n";
-    print(head); //print the elements from left to right so from smallest to biggest(left, root, right)
+    inorder(head, arr);
+    print(arr); //print the elements from left to right so from smallest to biggest(left, root, right)
     std::cout <<"\n";
+
+    arr.clear();
     std::cout << "The elements of the tree printed using Preorder:\n";
-    print_Preorder(head); //first print the root then the recursively the left side and then recursively the right side(root, left, left)
+    preorder(head, arr); //first print the root then the recursively the left side and then recursively the right side(root, left, left)
+    print(arr);
     std::cout <<"\n";
+
+    arr.clear();
     std::cout << "The elements of the tree printed using Postorder:\n";
-    print_Postorder(head); //first print recursively the left side, then the right side and then the root(left, right, root)
+    postorder(head, arr); //first print recursively the left side, then the right side and then the root(left, right, root)
+    print(arr);
     std::cout <<"\n";
     
     std::cout << "Minimum height of the tree: " << min_height(head)<<"\n";
@@ -176,14 +204,17 @@ int main() {
     std::cout << "The root node has a value of "<<find_root(p[rand()%ELEMENTS])->data<<"\n";
 
     
-    node* randomValue = new node();
-    randomValue->data = p[rand()%ELEMENTS]->data;
+    int randomValue = p[rand()%ELEMENTS]->data;
     node* temp = find_num(head, randomValue);
-    std::cout << "The value "<< randomValue->data<<" when put as an argument in the search function will return a node with the value of "<< temp->data<<" "<<((temp->parent!=nullptr)?("and parent with a value of "+std::to_string(temp->parent->data)):" and it is the head node")<<"\n";
+    std::cout << "The value "<< randomValue<<" when put as an argument in the search function will return a node with the value of "<< temp->data<<" "<<((temp->parent!=nullptr)?("and parent with a value of "+std::to_string(temp->parent->data)):" and it is the head node")<<"\n";
     
     int data = find_max(head)->data;
     del(find_max(head));
     std::cout << "After deleting "<<data<<" Inorder is:\n";
-    print(head);
+    arr.clear();
+    inorder(head, arr);
+    print(arr);
     return 0;
 }
+
+
