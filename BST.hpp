f3d3998be1d9ded7d2bf1c5207c0982a94b46a08
Fotios Vaitsopoulos <fotis.vaitsopoulos2008@gmail.com>
2025-03-23 00:15:24 +0100

    Converted into a class

---
+++ BST.hpp
@@ -0,0 +1,226 @@
+#include <vector>
+#include <algorithm>
+
+struct node {
+    int data;
+    node* left = nullptr;
+    node* right = nullptr;
+    node* parent = nullptr;
+
+    node(int d) {
+        data = d;
+    }
+
+    node() = default;
+};
+
+class BST {
+    private:
+        node* root = new node();
+
+        /* Returns minimum height of the tree
+        @param root root of the BST */
+        int min_height(node* root) {
+            if (root == nullptr) return 0;
+            return 1 + std::min(min_height(root->left), min_height(root->right));
+        }
+
+        /* Returns height of the tree
+        @param root root of the tree */
+        int height(node* root) {
+            if (root == nullptr) return 0;
+            return 1 + std::max(height(root->left), height(root->right));
+        }
+
+        /* Returns maximum valued node
+        @param root root of the tree */
+        node* find_max(node* root) {
+            if (root->right == nullptr) return root;
+            return find_max(root->right);
+        }
+
+        /* Returns minimum valued node
+        @param root root of the tree */
+        node* find_min(node* root) {
+            if (root->left == nullptr) return root;
+            return find_min(root->left);
+        }
+
+        /* Searches and returns a node, returns nullptr if node doesn't exist
+        @param root root of the tree 
+        @param key value that you are searching for */
+        node* find(node* root, int key) {
+            if (root == nullptr) return nullptr;
+            if (root->data > key) return find(root->left, key);
+            else if (root->data < key) return find(root->right, key);
+            return root;
+        }
+
+        /* Inorder representation(left, root, right)
+        @param root root of the tree
+        @param arr vector which holds the inorder representation */
+        void inorder(node* root, std::vector<int>& arr) {
+            if (root == nullptr) return;
+            inorder(root->left, arr);
+            arr.push_back(root->data);
+            inorder(root->right, arr);
+        }
+
+        /* Preorder representation(root, left, right)
+        @param root root of the tree
+        @param arr vector which holds the preorder representation */
+        void preorder(node* root, std::vector<int>& arr) {
+            if (root == nullptr) return;
+            arr.push_back(root->data);
+            preorder(root->left, arr);
+            preorder(root->right, arr);
+        }
+
+        /* Postorder representation(left, right, root)
+        @param root root of the tree
+        @param arr vector which holds the postorder representation */
+        void postorder(node* root, std::vector<int>& arr) {
+            if (root == nullptr) return;
+            postorder(root->left, arr);
+            postorder(root->right, arr);
+            arr.push_back(root->data);
+        }
+
+        /* Insert node in BST
+        @param root root of the tree
+        @param p node to be inserted */
+        void insert(node* &root, int p) {
+            if (p > root->data) {
+                if (root->right == nullptr) {
+                    node* t = new node(p);
+                    root->right = t;
+                    t->parent = root;
+                    return;
+                }
+                insert(root->right, p);
+            }
+            else {
+                if (root->left == nullptr) {
+                    node* t = new node(p);
+                    root->left = t;
+                    t->parent = root;
+                    return;
+                }
+                insert(root->left, p);
+            }
+        }
+    public:
+        BST(int data) {
+            this->root->data = data;
+        }
+
+        BST() = default;
+
+        /* Returns minimum height of the tree */
+        int min_height() {
+            return min_height(this->root);
+        }
+
+        /* Returns height of the tree */
+        int height() {
+            return height(this->root);
+        }
+
+        /* Returns maximum valued node */
+        node* find_max() {
+            return find_max(this->root);
+        }
+
+        /* Returns minimum valued node */
+        node* find_min() {
+            return find_min(this->root);
+        }
+
+        /* Searches and returns a node, returns nullptr if node doesn't exist
+        @param key value that you are searching for */
+        node* find(int key) {
+            return find(this->root, key);
+        }
+
+        /* Inorder representation(left, root, right)
+        @param arr vector which holds the inorder representation */
+        void inorder(std::vector<int>& arr) {
+            inorder(this->root, arr);
+        }
+
+        /* Preorder representation(root, left, right)
+        @param arr vector which holds the preorder representation */
+        void preorder(std::vector<int>& arr) {
+            preorder(this->root, arr);
+        }
+
+        /* Postorder representation(left, right, root)
+        @param arr vector which holds the postorder representation */
+        void postorder(std::vector<int>& arr) {
+            postorder(this->root, arr);
+        }
+
+        /* Insert node in BST
+        @param p node to be inserted */
+        void insert(int p) {
+            insert(this->root, p);
+        }
+
+        /* Returns root of the tree */
+        node* get_root() {
+            return this->root;
+        }
+
+        /* Delete node from the BST
+        @param n node to be deleted */
+        void del(node* n) {
+            if (n == nullptr) return;
+
+            if (n->left == nullptr && n->right == nullptr) {
+                if (n->parent == nullptr) return;
+                else {
+                    if (n->parent->left != nullptr && n->parent->left == n) {
+                        n->parent->left = nullptr;
+                    }
+                    else if (n->parent->right != nullptr && n->parent->right == n) {
+                        n->parent->right = nullptr;
+                    }
+                }
+            }
+            else if (n->left == nullptr) {
+                n->right->parent = n->parent;
+                if (n->parent != nullptr) {
+                    if (n->parent->left != nullptr && n->parent->left == n) n->parent->left = n->right;
+                    else if (n->parent->right != nullptr && n->parent->right == n) n->parent->right = n->right;
+                }
+            }
+            else if (n->right == nullptr) {
+                n->left->parent = n->parent;
+                if (n->parent != nullptr) {
+                    if (n->parent->left != nullptr && n->parent->left == n) n->parent->left = n->left;
+                    else if (n->parent->right != nullptr && n->parent->right == n) n->parent->right = n->left;
+                }
+            }
+            else {
+                node* replace = find_max(n->left);
+                if (replace->parent != n) {
+                    replace->left = n->left;
+                    n->left->parent = replace;
+                }
+
+                replace->right = n->right;
+                replace->parent = n->parent;
+
+                n->right->parent = replace;
+            }
+
+            delete n;
+        }
+    
+        /* Delete node from the BST
+        @param n value of node to be deleted */
+        void del(int n) {
+            del(find(n));
+        }
+
+};
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <random>
+#include "BST.hpp"
+
+#define print(a) for(int x: a) std::cout << x<<' ';
+
+
+int main() {
+    srand(time(0)); //this is just for the random numbers nothing to do with the trees
+    const int LIMIT = 100; //the range of the values that the nodes can have is from 0 to 100
+    const int ELEMENTS = 5;
+
+    BST* bst = new BST(rand()%LIMIT);
+
+    
+    std::cout << "The elements of the tree printed in the order they were inserted:\n";
+    std::cout << bst->get_root()->data<<"\n";
+
+    std::vector<int> p;
+    //now just populate the tree with the nodes
+    for (int i = 0; i<ELEMENTS; i++) {
+        p.push_back(rand()%LIMIT);
+        
+        bst->insert(p[i]); // insert
+
+        std::cout << p[i] <<"\n";
+    }
+    std::cout << "\n";
+
+    std::vector<int> arr;
+    std::cout << "The elements of the tree printed using Inorder:\n";
+    bst->inorder(arr);
+    print(arr); //print the elements from left to right so from smallest to biggest(left, root, right)
+    std::cout <<"\n";
+
+    arr.clear();
+    std::cout << "The elements of the tree printed using Preorder:\n";
+    bst->preorder(arr); //first print the root then the recursively the left side and then recursively the right side(root, left, left)
+    print(arr);
+    std::cout <<"\n";
+
+    arr.clear();
+    std::cout << "The elements of the tree printed using Postorder:\n";
+    bst->postorder(arr); //first print recursively the left side, then the right side and then the root(left, right, root)
+    print(arr);
+    std::cout <<"\n";
+    
+    std::cout << "Minimum height of the tree: " << bst->min_height()<<"\n";
+    
+    std::cout << "Maximum height of the tree: "<<bst->height()<<"\n";
+    
+    std::cout << "The greatest value in the tree: "<<bst->find_max()->data<<"\n";
+    
+    std::cout << "The smallest value in the tree: "<<bst->find_min()->data<<"\n";
+
+    std::cout << "The root node has a value of "<<bst->get_root()->data<<"\n";
+
+    
+    int randomValue = p[rand()%ELEMENTS];
+    node* temp = bst->find(randomValue);
+    if (temp == nullptr) {
+        std::cout << "Node not found\n";
+    }
+    else {
+        std::cout << "The value "<< randomValue<<" when put as an argument in the search function will return a node with the value of "<< temp->data<<" "<<((temp->parent!=nullptr)?("and parent with a value of "+std::to_string(temp->parent->data)):" and it is the head node")<<"\n";
+    }
+    
+    bst->del(randomValue);
+    std::cout << "After deleting "<<randomValue<<" Inorder is:\n";
+    arr.clear();
+    bst->inorder(arr);
+    print(arr);
+    return 0;
+}
+
+
